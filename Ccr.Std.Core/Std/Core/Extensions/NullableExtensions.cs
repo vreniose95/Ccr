e8023e175cd1c1d59566280400e35e5789e26924
vreniose95 <vreniose95@gmail.com>
2018-04-05 06:38:18 -0500

    nullableextension

---
+++ Ccr.Std.Core/Std/Core/Extensions/NullableExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ccr.Std.Core.Extensions
+{
+  public static class NullableExtensions
+  {
+    private static readonly IDictionary<Type, object> _defaultTypeInstanceMap
+      = new Dictionary<Type, object>
+      {
+        // ReSharper disable BuiltInTypeReferenceStyle
+        [typeof(Boolean)] = default(Boolean),
+        [typeof(Decimal)] = default(Decimal),
+        [typeof(Double)] = default(Double),
+        [typeof(Int16)] = default(Int16),
+        [typeof(Int32)] = default(Int32),
+        [typeof(Int64)] = default(Int64),
+        [typeof(SByte)] = default(SByte),
+        [typeof(Single)] = default(Single),
+        [typeof(UInt16)] = default(UInt16),
+        [typeof(UInt32)] = default(UInt32),
+        [typeof(UInt64)] = default(UInt64)
+        // ReSharper restore BuiltInTypeReferenceStyle
+      };
+
+    public static object CreateDefaultForType(
+      this Type @this)
+    {
+      if (@this.IsGenericOf(typeof(Nullable<>)))
+      {
+        @this = @this
+                .GetGenericArguments()
+                .Single();
+      }
+
+      if (!_defaultTypeInstanceMap.TryGetValue(
+        @this,
+        out var typeInstance))
+      {
+        return typeInstance;
+      }
+
+      var _default = @this.GetDefaultValue();
+      _defaultTypeInstanceMap.Add(
+        @this,
+        _default);
+
+      return _default;
+    }
+  }
+}
